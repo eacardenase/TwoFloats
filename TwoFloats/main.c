51b3e4c46b03606e4b54cc72c3646d1cfdc397d7
Edwin Cardenas <eacardenase@gmail.com>
2024-12-27 13:14:00 -0500

    feat: add program functionality

--- TwoFloats/main.c
+++ TwoFloats/main.c
@@ -8,7 +8,11 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    float pi = 3.1415;
+    float answerToLife = 42.0;
+    double product = pi * answerToLife;
+    
+    printf("The product is %f.\n", product);
+    
     return 0;
 }
