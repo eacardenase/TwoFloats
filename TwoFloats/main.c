23249bf3a2a60569953ba3e60672591e24763ae3
Edwin Cardenas <eacardenase@gmail.com>
2024-12-27 13:01:18 -0500

    Initial Commit

---
+++ TwoFloats/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Edwin Cardenas on 12/27/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
